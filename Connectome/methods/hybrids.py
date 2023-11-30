ec912d425a469835f257e513aaf5196d24348e0d
joan <j.roget@sanoscience.org>
2023-11-30 11:19:08 +0100

    1. Changed oedema to lesion

--- Connectome/methods/hybrids.py
+++ Connectome/methods/hybrids.py
@@ -93,15 +93,15 @@ def hybrid(config, f, acronym):
             os.system(f"dwi2response dhollander {nii_dwi_bc} {wm_res} {gm_res} {csf_res} -mask {mask_dwi}\
                         -fslgrad {bvec_dwi} {bval_dwi} -force -quiet -voxels {vox} ")
             
-        oedema_wm_res, oedema_gm_res, oedema_csf_res = inter_dir+'oedema_wm_response.txt', inter_dir+'oedema_gm_response.txt', inter_dir+'oedema_csf_response.txt'
-        if skip and os.path.exists(oedema_wm_res) and os.path.exists(oedema_gm_res) and os.path.exists(oedema_csf_res):
+        lesion_wm_res, lesion_gm_res, lesion_csf_res = inter_dir+'lesion_wm_response.txt', inter_dir+'lesion_gm_response.txt', inter_dir+'lesion_csf_response.txt'
+        if skip and os.path.exists(lesion_wm_res) and os.path.exists(lesion_gm_res) and os.path.exists(lesion_csf_res):
             logging.info(" " + subject_ID + " in " + session + " Response functions inside lesion already available... skiping")
         else:
             logging.info(" " + subject_ID + " in " + session + " Extracting the single shell response function")
-            extract_response_shell(wm_res, oedema_wm_res, gm_res, oedema_gm_res, csf_res, oedema_csf_res, config_shell=config['shell'])
+            extract_response_shell(wm_res, lesion_wm_res, gm_res, lesion_gm_res, csf_res, lesion_csf_res, config_shell=config['shell'])
 
         ####################################
-        ### Reconstruction INSIDE oedema ###
+        ### Reconstruction INSIDE lesion ###
         ####################################
         ### Extract b0 + single shell ###
         nii_dwi_sshell, ss_bvec, ss_bval = inter_dir+'dwi_single_shell.mif', inter_dir+'bvec_single_shell.bvec', inter_dir+'bval_single_shell.bval'
@@ -114,24 +114,24 @@ def hybrid(config, f, acronym):
             os.system(f"mrconvert {nii_dwi_sshell} -fslgrad {ss_bvec} {ss_bval} {nii_dwi_sshell} -force -quiet ")
         
         ### Run the reconstruction algorithm ###
-        oedema_wm_fod, oedema_gm_fod, oedema_csf_fod = inter_dir+'oedema_wm_fod.mif', inter_dir+'oedema_gm_fod.mif', inter_dir+'oedema_csf_fod.mif'
-        oedema_wm_norm, oedema_gm_norm, oedema_csf_norm = inter_dir+'oedema_wm_fod_norm.mif', inter_dir+'oedema_gm_fod_norm.mif', inter_dir+'oedema_csf_fod_norm.mif'
-        if skip and os.path.exists(oedema_wm_norm):
+        lesion_wm_fod, lesion_gm_fod, lesion_csf_fod = inter_dir+'lesion_wm_fod.mif', inter_dir+'lesion_gm_fod.mif', inter_dir+'lesion_csf_fod.mif'
+        lesion_wm_norm, lesion_gm_norm, lesion_csf_norm = inter_dir+'lesion_wm_fod_norm.mif', inter_dir+'lesion_gm_fod_norm.mif', inter_dir+'lesion_csf_fod_norm.mif'
+        if skip and os.path.exists(lesion_wm_norm):
             logging.info(" " + subject_ID + " in " + session + " fODFs inside lesion already available... skiping")
         else:
             logging.info(" " + subject_ID + " in " + session + " Reconstructing fODFs with SS3T-CSD inside lesion")
-            os.system(f"ss3t_csd_beta1_3T {nii_dwi_sshell} {oedema_wm_res} {oedema_wm_fod} {oedema_gm_res} {oedema_gm_fod} {oedema_csf_res} {oedema_csf_fod} \
+            os.system(f"ss3t_csd_beta1_3T {nii_dwi_sshell} {lesion_wm_res} {lesion_wm_fod} {lesion_gm_res} {lesion_gm_fod} {lesion_csf_res} {lesion_csf_fod} \
                  -mask {tumor_dwi} -force -quiet ")
         
         ### Tissue component normalization ### (Aerts, et al. eNeuro 2018)
-        if skip and os.path.exists(oedema_wm_norm):
+        if skip and os.path.exists(lesion_wm_norm):
             logging.info(" " + subject_ID + " in " + session + " Normalized fODFs inside lesion already available... skiping")
         else:
             logging.info(" " + subject_ID + " in " + session + " Normalizing fODFs inside lesion")
-            os.system(f"mtnormalise {oedema_wm_fod} {oedema_wm_norm} {oedema_gm_fod} {oedema_gm_norm} {oedema_csf_fod} {oedema_csf_norm} -mask {tumor_dwi} -force -quiet ")
+            os.system(f"mtnormalise {lesion_wm_fod} {lesion_wm_norm} {lesion_gm_fod} {lesion_gm_norm} {lesion_csf_fod} {lesion_csf_norm} -mask {tumor_dwi} -force -quiet ")
         
         #####################################
-        ### Reconstruction OUTSIDE oedema ###
+        ### Reconstruction OUTSIDE lesion ###
         #####################################
         ### Run the reconstruction algorithm ###
         wm_fod, gm_fod, csf_fod = inter_dir+'wm_fod.mif', inter_dir+'gm_fod.mif', inter_dir+'csf_fod.mif'
@@ -158,21 +158,21 @@ def hybrid(config, f, acronym):
              wm_merged = inter_dir + 'FODs.mif'
         else:
             logging.info(" " + subject_ID + " in " + session + " Merging WM fODFs reconstructions")
-            oedema_norm_nii, norm_nii = inter_dir+'oedema_wm_fod_norm.nii.gz', inter_dir+'wm_fod_norm.nii.gz'
-            os.system(f"mrconvert {oedema_wm_norm} {oedema_norm_nii} -quiet -force ")
+            lesion_norm_nii, norm_nii = inter_dir+'lesion_wm_fod_norm.nii.gz', inter_dir+'wm_fod_norm.nii.gz'
+            os.system(f"mrconvert {lesion_wm_norm} {lesion_norm_nii} -quiet -force ")
             os.system(f"mrconvert {wm_norm} {norm_nii} -quiet -force ")
-            wm_merged = merge_fods(norm_nii, oedema_norm_nii, inter_dir)
+            wm_merged = merge_fods(norm_nii, lesion_norm_nii, inter_dir)
         
         ###############
         ### Seeding ###
         ###############
         ### Generate the seeding masks for the tractography ###
         if config['trac']['seeding'] == "random":
-            oedema_tck_seeding = "-seed_image " + tumor_dwi
+            lesion_tck_seeding = "-seed_image " + tumor_dwi
             healthy_tck_seeding = "-seed_image " + mask_dwi
         elif config['trac']['seeding'] == "dynamic": # Dynamic seeding
-            oedema_seeding_mask = oedema_wm_norm 
-            oedema_tck_seeding = "-seed_dynamic " + oedema_seeding_mask
+            lesion_seeding_mask = lesion_wm_norm 
+            lesion_tck_seeding = "-seed_dynamic " + lesion_seeding_mask
             healthy_seeding_mask = wm_norm
             healthy_tck_seeding = "-seed_dynamic " + healthy_seeding_mask
         else:
@@ -197,26 +197,26 @@ def hybrid(config, f, acronym):
         ### Tracking within lesion ###
         ##############################
         ### Run tractography through lesion without ACT ###
-        oedema_tck_file = output_dir + subject_ID + '_' + session + '_trac-' + streams_lesion + '_lesion.tck'
-        if skip and os.path.exists(oedema_tck_file):
+        lesion_tck_file = output_dir + subject_ID + '_' + session + '_trac-' + streams_lesion + '_lesion.tck'
+        if skip and os.path.exists(lesion_tck_file):
             logging.info(" " + subject_ID + " in " + session + " Lesion tractogram already available... skiping")
         else:
             logging.info(" " + subject_ID + " in " + session + " Generating and filtering tractogram through lesion")
             # We use the merged fod files to capture tracts that pass through the lesion
             # We seed only inside the lesion
-            os.system(f"tckgen -algorithm iFOD2 {oedema_tck_seeding} -backtrack -select {streams_lesion} \
+            os.system(f"tckgen -algorithm iFOD2 {lesion_tck_seeding} -backtrack -select {streams_lesion} \
                 -minlength {t_config['min_len']} -maxlength {t_config['max_len']} \
                 -fslgrad {bvec_dwi} {bval_dwi} -mask {whole_t1_mask} -cutoff {config['trac']['cutoff']} \
-                -force -quiet  {wm_merged} {oedema_tck_file}")    
+                -force -quiet  {wm_merged} {lesion_tck_file}")    
         
         ### SIF2 to match the lesion underlying diffusion signal ###
-        oedema_weights_sift = output_dir + subject_ID + '_' + session + '_trac-' + streams_lesion + '_SIFT2-weights_tkh-' + t_config['sift2_tikhonov'] + '_tv-' + t_config['sift2_tv'] + '_lesion.txt'
-        if skip and os.path.exists(oedema_weights_sift):
+        lesion_weights_sift = output_dir + subject_ID + '_' + session + '_trac-' + streams_lesion + '_SIFT2-weights_tkh-' + t_config['sift2_tikhonov'] + '_tv-' + t_config['sift2_tv'] + '_lesion.txt'
+        if skip and os.path.exists(lesion_weights_sift):
             logging.info(" " + subject_ID + " in " + session + " Filtered tractogram already available... skiping")
         else:
             logging.info(" " + subject_ID + " in " + session + " Filtering tractogram")
             # We filter with fODFs from the lesion area 
-            os.system(f"tcksift2 {oedema_tck_file} {oedema_wm_norm} {oedema_weights_sift} -fd_scale_gm \
+            os.system(f"tcksift2 {lesion_tck_file} {lesion_wm_norm} {lesion_weights_sift} -fd_scale_gm \
                     -reg_tikhonov {t_config['sift2_tikhonov']} -reg_tv {t_config['sift2_tv']} -force -quiet")
         
         ###############################
@@ -248,7 +248,7 @@ def hybrid(config, f, acronym):
         if t_config['save_trk']:
             logging.info(" " + subject_ID + " in " + session + " Converting to .trk")
             tck2trk(nii_t1, healthy_tck_file)
-            tck2trk(nii_t1, oedema_tck_file)
+            tck2trk(nii_t1, lesion_tck_file)
 
         ###########################################
         ### Merging strategies and connectomics ###
@@ -256,8 +256,8 @@ def hybrid(config, f, acronym):
         ### Generate Connectomes ###
         healthy_cm_file = output_dir + subject_ID + '_' + session + '_healthy_CM.csv'
         healthy_cm2tck = output_dir + subject_ID + '_' + session + '_healthy_cm2trac.txt'
-        oedema_cm_file = output_dir + subject_ID + '_' + session + '_lesion_CM.csv'
-        oedema_cm2tck = output_dir + subject_ID + '_' + session + '_lesion_cm2trac.txt'
+        lesion_cm_file = output_dir + subject_ID + '_' + session + '_lesion_CM.csv'
+        lesion_cm2tck = output_dir + subject_ID + '_' + session + '_lesion_cm2trac.txt'
         merged_cm_file = output_dir + subject_ID + '_' + session + '_CM.csv'
         #merged_cm2tck = output_dir + subject_ID + '_' + session + '_cm2trac.txt'
         if config["space"] == 'MNI':
@@ -273,12 +273,12 @@ def hybrid(config, f, acronym):
             os.system(f"tck2connectome {healthy_tck_file} {atlas_path} {healthy_cm_file} -tck_weights_in {healthy_weights_sift} \
                         -symmetric -zero_diagonal -out_assignments {healthy_cm2tck} -force -quiet ")
         ### Lesion Connectome ###
-        if skip and os.path.exists(oedema_cm_file):
+        if skip and os.path.exists(lesion_cm_file):
             logging.info(" " + subject_ID + " in " + session + " Lesion connectome already available... skiping")
         else:
             logging.info(" " + subject_ID + " in " + session + " Generating lesion connectome")
-            os.system(f"tck2connectome {oedema_tck_file} {atlas_path} {oedema_cm_file} -tck_weights_in {oedema_weights_sift} \
-                        -symmetric -zero_diagonal -out_assignments {oedema_cm2tck} -force -quiet ")
+            os.system(f"tck2connectome {lesion_tck_file} {atlas_path} {lesion_cm_file} -tck_weights_in {lesion_weights_sift} \
+                        -symmetric -zero_diagonal -out_assignments {lesion_cm2tck} -force -quiet ")
         ### Merging Connectomes ###
         if skip and os.path.exists(merged_cm_file):
             logging.info(" " + subject_ID + " in " + session + " Merged connectome already available... skiping")
@@ -287,7 +287,7 @@ def hybrid(config, f, acronym):
             # TODO: Matrix merging strategy
                 # 1. Greedy approach
                 # 2. Another alternative? Difficult to justify
-            merge_connectivity(healthy_cm_file, oedema_cm_file, merged_cm_file, strategy='greedy')
+            merge_connectivity(healthy_cm_file, lesion_cm_file, merged_cm_file, strategy='greedy')
 
         ### Structural connectivity stats ###
         if skip and os.path.exists(output_dir + '_' + subject_ID + session + '.png'):
@@ -297,7 +297,7 @@ def hybrid(config, f, acronym):
             sg = GraphFromCSV(healthy_cm_file, subject_ID+'_'+session+'_healthy', output_dir)
             sg.flatten_graph(save=True) 
             sg.process_graph()
-            sg = GraphFromCSV(oedema_cm_file, subject_ID+'_'+session+'_lesion', output_dir)
+            sg = GraphFromCSV(lesion_cm_file, subject_ID+'_'+session+'_lesion', output_dir)
             sg.flatten_graph(save=True) 
             sg.process_graph()
             sg = GraphFromCSV(merged_cm_file, subject_ID+'_'+session, output_dir)
@@ -307,21 +307,21 @@ def hybrid(config, f, acronym):
         ### Remove .tck heavy file ###
         if not t_config["keep_full_tck"] and os.path.exists(healthy_tck_file):
             os.remove(healthy_tck_file)
-        if not t_config["keep_full_tck"] and os.path.exists(oedema_tck_file):
-            os.remove(oedema_tck_file)
+        if not t_config["keep_full_tck"] and os.path.exists(lesion_tck_file):
+            os.remove(lesion_tck_file)
 
         ### Remove intermediates ###
         if config["delete"]["response_funcs"]:
-            os.system(f"rm {wm_res} {gm_res} {csf_res} {vox} {oedema_wm_res} {oedema_gm_res} {oedema_csf_res}")
+            os.system(f"rm {wm_res} {gm_res} {csf_res} {vox} {lesion_wm_res} {lesion_gm_res} {lesion_csf_res}")
         if config["delete"]["fODFs"]:
             os.system(f"rm {wm_fod} {gm_fod} {csf_fod} {gm_norm} {csf_norm} \
-                {oedema_wm_fod} {oedema_gm_fod} {oedema_csf_fod} {oedema_gm_norm} {oedema_csf_norm}")
+                {lesion_wm_fod} {lesion_gm_fod} {lesion_csf_fod} {lesion_gm_norm} {lesion_csf_norm}")
         if config["delete"]["WM_fODF"]:
-            os.system(f"rm {wm_norm} {oedema_wm_norm} {wm_merged}")
+            os.system(f"rm {wm_norm} {lesion_wm_norm} {wm_merged}")
         if config["delete"]["seeding_mask"] and os.path.exists(healthy_seeding_mask):
             os.system(f"rm {healthy_seeding_mask}")
-        if config["delete"]["seeding_mask"] and os.path.exists(oedema_seeding_mask):
-            os.system(f"rm {oedema_seeding_mask}")
+        if config["delete"]["seeding_mask"] and os.path.exists(lesion_seeding_mask):
+            os.system(f"rm {lesion_seeding_mask}")
         if config["delete"]["bias_corr"]:
             os.system(f"rm {nii_dwi_bc}")
         if config["delete"]["seg_5tt"]:
